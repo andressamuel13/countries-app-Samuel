b4795e46960299c20bab0e3bc5de75d2c53c5cf0
samuel <samuelma236@gmail.com>
2025-04-29 20:32:09 -0500

    agregada funcion para mostrar todos los paises en la targeta

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 import { countries } from './countries.js'
 
-console.log(countries)
+const container = document.querySelector('.country-card');
+
+countries.map(({ country, capital }) => {
+  const div = document.createElement('div');
+  div.className = 'country';
+  div.innerHTML = `<h2>${country}</h2><p>Capital: ${capital}</p>`;
+  container.appendChild(div);
+});
+
